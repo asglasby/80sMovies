6c31546b71075761b1ad2f31d93406ac267b1178
Mahopa <mahopa@gmail.com>
2014-04-16 15:53:59 -0500

    80s Movies

---
+++ 80sMovies/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("_80sMovies")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_80sMovies")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("baac04b1-1272-4cba-baef-44f0ab824d69")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 80sMovies/Scripts/_Movies.js
@@ -0,0 +1,71 @@
+﻿var myApp = angular.module('myApp', ['ngRoute']);
+myApp.URL = "https://domo.firebaseio.com/.json";
+myApp.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "Views/home.html",
+        controller: 'HomeCtrl'
+    })
+    .when("/editMovie/:id", {
+        templateUrl: "Views/AddMovie.html",
+        controller: 'editMovie'
+    })
+    .when("/NewMovie", {
+        templateUrl: "Views/AddMovie.html",
+        controller: 'addMovie'
+    }).otherwise({
+        templateUrl: "Views/Error.html"
+    });
+});
+myApp.factory('Movies', function () {
+    var movieArray = [{ title: "Top Gun", year: 1986, director: "Someone" },
+        { title: "Red Dawn", director: "Tim", year: 1995 },
+        { title: "Ghost Busters", year: 1986, director: "Other" }];
+    return movieArray;
+});
+myApp.filter('Reverse', function () {
+    return function (text) {
+        return text.split("").reverse().join("");
+    }
+});
+myApp.controller('HomeCtrl', function ($scope, Movies) {
+    $scope.Movies = Movies;
+    $scope.Options = ["title", "director", "year"];
+    //$scope.SearchBy = {};
+    $scope.search = function (item) {
+        for (var x in $scope.SearchBy) {
+            if ($scope.SearchBy[x]) {
+                if (item[x].toString().toLowerCase().indexOf($scope.query.toLowerCase()) != -1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+        //if (
+        //    item.director.indexOf($scope.query) != -1 ||
+        //   item.title.indexOf($scope.query) != -1) {
+        //    return true;
+        //}
+        //else { return false; }
+    };
+});
+myApp.controller('editMovie', function ($scope, Movies, $location, $routeParams) {
+    $scope.Movie = Movies[$routeParams.id];
+    $scope.addMovie = function (Movie) {
+        $location.path('/');
+    };
+    $scope.cancel = function () {
+        $scope.Movie = MovieCopy;
+        alert(MovieCopy);
+        $location.path('/');
+    };
+});
+myApp.controller('addMovie', function ($scope, $http, Movies, $location) {
+    $scope.Movies = Movies;
+    $scope.addMovie = function (Movie) {
+        $scope.Movies.push(Movie);
+        $scope.title = "";
+        $scope.year = "";
+        $scope.director = "";
+        $location.path('/');
+    };
+});
